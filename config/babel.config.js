9d2b101f7f0032b9795a2f68d90ce973047057d0
duanhaochen <duanhaochen@126.com>
2018-05-12 13:32:54 +0800

    修改babel配置

--- config/babel.config.js
+++ config/babel.config.js
@@ -39,9 +39,15 @@ module.exports = {
       '@babel/preset-react'
     ],
     plugins: [
-      '@babel/plugin-proposal-decorators',
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true
+        }
+      ],
       '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-do-expressions',
+      '@babel/plugin-proposal-optional-chaining',
       [
         'import',
         {
